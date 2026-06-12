0116f2d9e2e80a0035d2198950cfa982dfb218f1
miken515 <515.miken@gmail.com>
2026-06-11 21:56:03 -0500

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # create map
+        previousMap = {}
+
+        for i, n in enumerate(nums):
+            print('Previous Map:', previousMap)
+            print('I,N', i, n)
+            difference = target - n
+            print('Target', target)
+            print('Diff', difference)
+            if difference in previousMap:
+                return[previousMap[difference], i]
+            previousMap[n] = i 
+        return
