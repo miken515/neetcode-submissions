02701c1ee11b74ddc67568b2d45799e19aedd5e8
miken515 <515.miken@gmail.com>
2026-06-26 18:08:25 -0500

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,10 @@
+# recursive
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one = 1
+        two = 1
+        for i in range(n - 1):
+            tmp = one
+            one = one + two
+            two = tmp
+        return one
